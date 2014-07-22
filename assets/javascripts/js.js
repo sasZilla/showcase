84f1bee9cbaea7e8b12a9f0812327db5d5b0ac22
unknown <a.osipov@aosipov-pc.i.drweb.ru>
2014-07-22 15:57:50 +0400

    use ajax to get json data

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -2,14 +2,6 @@
 	var cache = {};
 
 	var Utils = {
-		ajax: function(url, callback) {
-			var script = document.createElement('script');
-				script.type = 'text/javascript';
-				script.src = url + '?callback=parseData';
-
-			document.body.appendChild(script);
-		},
-
 		tmpl: function(str, data) {
 			// Figure out if we're getting a template, or if we need to
 			// load the template - and be sure to cache the result.
@@ -66,9 +58,10 @@
 					struct.$container.append(struct.tmpl);
 					struct.$container.append('<div class="shadow"></div>');
 
+					// next tick
 					setTimeout(function() {
 						$(struct.__shadowSelector).height( $(document).height() )
-					}, 10);
+					}, 0);
 				},
 				destroy: function() {
 					$(struct.__modalSelector).remove();
@@ -89,8 +82,6 @@
 			this.productImg = '.productThumb';
 
 			this.getJson();
-			this.layout();
-			this.bind();
 		},
 
 		bind: function() {
@@ -108,54 +99,18 @@
 					return el.id === id;
 				})[0];
 
-			if (typeof product !== " undefined") {
+			if (typeof product !== "undefined") {
 				Utils.dialog( Catalog.fullDescTemplate({product: product}) );
 			}
 		},
 
 		getJson: function() {
-			Catalog.__data = [
-								{
-									"id": 1,
-									"name": "Бинокль Veber WP UC (Ultra Compact) 10x42",
-									"descrSmall": "бинокль Veber WP UC 10x42",
-									"descLarge": "Бинокль Veber UC 10x42 WP – компактный надежный бинокль на основе призм порро. Стеклянная оптика имеет многослойное просветление, что обеспечивает яркость и насыщенность изображения. Бинокль имеет близкую точку фокусировки (около 2х метров), что позволит рассмотреть детали объектов, находящихся довольно близко, в то же время, увеличения в 10 крат позволят увидеть и те объекты, которые находятся на значительном удалении.",
-									"imgThumb": "img/1_thumb.jpg",
-									"imgLarge": "img/1_large.jpg"
-								},
-								{
-									"id": 2,
-									"name": "Бинокль Veber WP UC (Ultra Compact) 10x42",
-									"descrSmall": "бинокль Veber WP UC 10x42",
-									"descLarge": "Бинокль Veber UC 10x42 WP – компактный надежный бинокль на основе призм порро. Стеклянная оптика имеет многослойное просветление, что обеспечивает яркость и насыщенность изображения. Бинокль имеет близкую точку фокусировки (около 2х метров), что позволит рассмотреть детали объектов, находящихся довольно близко, в то же время, увеличения в 10 крат позволят увидеть и те объекты, которые находятся на значительном удалении.",
-									"imgThumb": "img/2_thumb.jpg",
-									"imgLarge": "img/2_large.jpg"
-								},
-								{
-									"id": 3,
-									"name": "Бинокль Veber WP UC (Ultra Compact) 10x42",
-									"descrSmall": "бинокль Veber WP UC 10x42",
-									"descLarge": "Бинокль Veber UC 10x42 WP – компактный надежный бинокль на основе призм порро. Стеклянная оптика имеет многослойное просветление, что обеспечивает яркость и насыщенность изображения. Бинокль имеет близкую точку фокусировки (около 2х метров), что позволит рассмотреть детали объектов, находящихся довольно близко, в то же время, увеличения в 10 крат позволят увидеть и те объекты, которые находятся на значительном удалении.",
-									"imgThumb": "img/3_thumb.jpg",
-									"imgLarge": "img/3_large.jpg"
-								},
-								{
-									"id": 4,
-									"name": "Бинокль Veber WP UC (Ultra Compact) 10x42",
-									"descrSmall": "бинокль Veber WP UC 10x42",
-									"descLarge": "Бинокль Veber UC 10x42 WP – компактный надежный бинокль на основе призм порро. Стеклянная оптика имеет многослойное просветление, что обеспечивает яркость и насыщенность изображения. Бинокль имеет близкую точку фокусировки (около 2х метров), что позволит рассмотреть детали объектов, находящихся довольно близко, в то же время, увеличения в 10 крат позволят увидеть и те объекты, которые находятся на значительном удалении.",
-									"imgThumb": "img/4_thumb.jpg",
-									"imgLarge": "img/4_large.jpg"
-								},
-								{
-									"id": 5,
-									"name": "Бинокль Veber WP UC (Ultra Compact) 10x42",
-									"descrSmall": "бинокль Veber WP UC 10x42",
-									"descLarge": "Бинокль Veber UC 10x42 WP – компактный надежный бинокль на основе призм порро. Стеклянная оптика имеет многослойное просветление, что обеспечивает яркость и насыщенность изображения. Бинокль имеет близкую точку фокусировки (около 2х метров), что позволит рассмотреть детали объектов, находящихся довольно близко, в то же время, увеличения в 10 крат позволят увидеть и те объекты, которые находятся на значительном удалении.",
-									"imgThumb": "img/5_thumb.jpg",
-									"imgLarge": "img/5_large.jpg"
-								}
-							];
+			$.getJSON('server/goods.json', function(jsonData) {
+				Catalog.__data = jsonData;
+
+				Catalog.layout();
+				Catalog.bind();
+			});
 		}
 	};
 
